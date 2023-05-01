aa194f524ea8b9dbbfb1776597215d18ef68615c
CSY54 <18496305+CSY54@users.noreply.github.com>
2023-05-01 16:39:55 +0800

    fix: history button in GitLab 15.10.1-ee

--- gitlab-history-button.user.js
+++ gitlab-history-button.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GitLab History Button
 // @namespace    https://gitlab.com
-// @version      0.1
+// @version      0.2
 // @description  Show history button on project page
 // @author       CSY54
 // @include      https://gitlab.*
@@ -11,7 +11,7 @@
 // @downloadURL  https://github.com/CSY54/tampermonkey-userscripts/raw/master/gitlab-history-button.user.js
 // ==/UserScript==
 
-(function() {
+(async function() {
     'use strict';
 
     const repoName = document.querySelector('.home-panel-title');
@@ -19,9 +19,36 @@
         return;
     }
 
-    const path = window.location.pathname;
-    const currentBranch = document.querySelector('.project-refs-form').querySelector('.dropdown-toggle-text').textContent;
-    const historyPath = new URL(`${path}/-/commits/${currentBranch}/`, window.location.href).pathname
-    const button = `<a href="${historyPath}" class="btn btn-default btn-md gl-button"><span class="gl-button-text">History</span></a>`
-    document.querySelector('.tree-controls div').children[0].insertAdjacentHTML('beforebegin', button)
+    async function until(cb) {
+        const RETRY_LIMIT = 60;
+        let retryCount = 0;
+        return new Promise((res, rej) => {
+            const id = setInterval(() => {
+                if (cb()) {
+                    clearInterval(id);
+                    res();
+                }
+
+                retryCount++;
+                if (retryCount > RETRY_LIMIT) {
+                    clearInterval(id);
+                    rej();
+                }
+            }, 1000);
+        });
+    }
+
+    until(() => {
+        try {
+            return document.querySelector('.tree-ref-holder button').textContent.trim() !== '';
+        } catch {
+            return false;
+        }
+    }).then(() => {
+        const path = window.location.pathname;
+        const currentBranch = document.querySelector('.tree-ref-holder button').textContent.trim()
+        const historyPath = new URL(`${path}/-/commits/${currentBranch}/`, window.location.href).pathname
+        const button = `<a href="${historyPath}" class="btn btn-default btn-md gl-button"><span class="gl-button-text">History</span></a>`
+        document.querySelector('.tree-controls > div').children[1].insertAdjacentHTML('beforebegin', button)
+    }).catch(() => {})
 })();
